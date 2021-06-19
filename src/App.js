31f05c47291147d84ffd2545ed9d6b78423a11e9
as0387 <as0387@naver.com>
2021-06-19 18:43:52 +0900

    소셜 로그인 진짜 끝

--- src/App.js
+++ src/App.js
@@ -7,31 +7,34 @@ import LoginPage from "./login/index";
 import RegisterPage from "./register/index";
 import { Switch, Route, Link, useHistory } from "react-router-dom";
 import { Button, Affix, Menu, Dropdown } from "antd";
-import { DownloadOutlined, DownOutlined, PlusOutlined } from "@ant-design/icons";
+import {
+  DownloadOutlined,
+  DownOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
 import { useEffect } from "react";
 import { login, logout } from "./store";
 import { useDispatch, useSelector } from "react-redux";
-
 
 function App() {
   const history = useHistory();
   const dispatch = useDispatch();
 
   const isLogin = useSelector((store) => store.isLogin);
-  const onClick= function () {
-                    history.push("/upload");
-                  }
-  // 
+  const onClick = function () {
+    history.push("/upload");
+  };
+  //
   const menu = (
-  <Menu >
-    <Menu.Item onClick={onClick} key="1" icon={<PlusOutlined />}>
-      일반상품
-    </Menu.Item>
-    <Menu.Item key="2" icon={<PlusOutlined />}>
-      경매상품
-    </Menu.Item>
-  </Menu>
-);
+    <Menu>
+      <Menu.Item onClick={onClick} key="1" icon={<PlusOutlined />}>
+        일반상품
+      </Menu.Item>
+      <Menu.Item key="2" icon={<PlusOutlined />}>
+        경매상품
+      </Menu.Item>
+    </Menu>
+  );
   const logoutProc = () => {
     localStorage.removeItem("Authorization");
     dispatch(logout());
@@ -45,63 +48,58 @@ function App() {
   }, []);
 
   return (
-    
     <div>
       <Affix offsetTop={0}>
-      <div id="header">
-        <div id="header-area">
-          <Link to="/">
-            <img src="/images/icons/잇츠마인.png" width="150" />
-          </Link>
-          {isLogin ? (
-            <>
-              <div>
-                <Dropdown overlay={menu}>
-                <Button
-                  className="k-button"
-                  size="large"
-                  
-                  icon={<DownOutlined />}
-                >
-                  상품 업로드
-                </Button>
-                </Dropdown>
-                <Button size="large" onClick={logoutProc}>
-                  로그아웃
-                </Button>
-              </div>
-              
-            </>
-            
-          ) : (
-            <>
-              <div>
-                
-                <Button
-                  className="k-button"
-                  size="large"
-                  onClick={function () {
-                    history.push("/login");
-                  }}
-                >
-                  로그인
-                </Button>
+        <div id="header">
+          <div id="header-area">
+            <Link to="/">
+              <img src="/images/icons/잇츠마인.png" width="150" />
+            </Link>
+            {isLogin ? (
+              <>
+                <div>
+                  <Dropdown overlay={menu}>
+                    <Button
+                      className="k-button"
+                      size="large"
+                      icon={<DownOutlined />}
+                    >
+                      상품 업로드
+                    </Button>
+                  </Dropdown>
+                  <Button size="large" onClick={logoutProc}>
+                    로그아웃
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <Button
+                    className="k-button"
+                    size="large"
+                    onClick={function () {
+                      history.push("/login");
+                    }}
+                  >
+                    로그인
+                  </Button>
 
-                <Button
-                  size="large"
-                  onClick={function () {
-                    history.push("/register");
-                  }}
-                >
-                  회원가입
-                </Button>
-              </div>
-            </>
-          )}
+                  <Button
+                    size="large"
+                    onClick={function () {
+                      history.push("/register");
+                    }}
+                  >
+                    회원가입
+                  </Button>
+                </div>
+              </>
+            )}
+          </div>
         </div>
-      </div>
       </Affix>
-      
+
       <div id="body">
         <Switch>
           <Route exact={true} path="/">
