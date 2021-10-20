104ef77176892650507ea3fe1a56bf055a6357e3
임동혁 <as0387@naver.com>
2021-10-20 22:52:00 +0900

    소캣통신 채팅

--- src/chat/index.js
+++ src/chat/index.js
@@ -4,26 +4,76 @@ import React from "react";
 import { useState } from "react";
 import "./index.css";
 import { API_URL } from "../config/constants";
-import dayjs from "dayjs";
-import { Link } from "react-router-dom";
 import { Spin, Space, Form, Button, Input } from "antd";
 import {} from "react-bootstrap";
-import { valueToNode } from "@babel/types";
-import { useCallback } from "react";
-console.log("");
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 
+var chatroomid;
+var opponentUserName;
+var stompClient = null;
+var sender;
+
+const connect = (values) => {
+  disconnect();
+  var socket = new SockJS("/talk");
+  stompClient = Stomp.over(socket);
+  chatroomid = values.chatroomid;
+  opponentUserName = values.opponentUserName;
+  sender = values.sender;
+  stompClient.connect({}, function () {
+    stompClient.subscribe("/topic/" + sender, function (e) {
+      showMessageLeft(JSON.parse(e.body));
+      console.log("새로운 메세지가 왔습니다.");
+    });
+  });
+};
+
+const onClickPurchase = (values) => {
+  var data = {
+    chatRoomId: chatroomid,
+    sender: sender,
+    receiver: opponentUserName,
+    message: values.message,
+  };
+  stompClient.send("/app/talk/send", {}, JSON.stringify(data));
+  showMessageRight(data);
+};
+
+function disconnect() {
+  if (stompClient !== null) {
+    stompClient.disconnect();
+  }
+}
+
+var space;
+function showMessageLeft(e) {
+  space = document.getElementById("chat-content");
+  let receivedBox = document.createElement("div");
+  receivedBox.innerHTML = `<li><span class"chat-box">${e.sender}:${e.message}</span></li>`;
+  space.append(receivedBox);
+
+  space.scrollTop = space.scrollHeight;
+}
+function showMessageRight(e) {
+  space = document.getElementById("chat-content");
+  let receivedBox = document.createElement("div");
+  receivedBox.innerHTML = `<li><span class="chat-box mine">${e.sender}: ${e.message}</span></li>`;
+  space.append(receivedBox);
+  space.scrollTop = space.scrollHeight;
+}
+
+window.onbeforeunload = function (e) {
+  disconnect();
+};
 function ChatPage() {
   const config = {
     headers: { Authorization: localStorage.getItem("Authorization") },
   };
 
-  var chatroomid;
-  const history = useHistory();
-  const [user, setUser] = useState(null);
-  const [chats, setChat] = React.useState([]);
-  const [messages, setMessages] = React.useState([]);
-  const [, updatteState] = useState();
-  const forceUpdate = useCallback(() => updatteState({}), []);
+  const [user, setUser] = useState();
+  const [chats, setChat] = useState([]);
+  const [messages, setMessages] = useState([]);
 
   React.useEffect(function () {
     axios
@@ -48,39 +98,36 @@ function ChatPage() {
       });
   }, []);
 
-  const chatRoom = () => {
-    console.log(chatroomid);
+  const chatRoom = (e) => {
+    console.log("chatRoom 실행");
+    var a1 = Number(e.target.getAttribute("data-msg1"));
+    var a2 = e.target.getAttribute("data-msg2");
+    var a3 = e.target.getAttribute("data-msg3");
+    console.log(a1, a2, a3);
+    var data = {
+      chatroomid: a1,
+      opponentUserName: a2,
+      sender: a3,
+    };
+    connect(data);
     axios
       .get(`${API_URL}/chat/${chatroomid}`, config)
       .then((result) => {
         console.log(result.data.messages);
         //실제 데이터로 변경
         setMessages(result.data.messages);
-        var objDiv = document.getElementById("chat-content");
 
+        var div = document.querySelector("div"); //제거하고자 하는 엘리먼트
+        var content = document.getElementById("chat-content"); // 그 엘리먼트의 부모 객체
+        content.removeChild(div);
+
+        var objDiv = document.getElementById("chat-content");
         objDiv.scrollTop = objDiv.scrollHeight;
       })
       .catch((error) => {
         console.error("에러발생!!", error);
       });
     return true;
-  };
-
-  const onClickPurchase = (values) => {
-    console.log(values.message);
-    axios.post(
-      `${API_URL}/chat`,
-      { writerId: user.id, roomId: chatroomid, message: values.message },
-      config
-    );
-    chatRoom()
-      .then((result) => {
-        chatRoom();
-        console.log(result);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
   };
 
   if (messages === null) {
@@ -97,48 +144,44 @@ function ChatPage() {
 
   return (
     <>
-      <div class="container p-4 detail">
-        <div class="row">
-          <div class="col-3 p-0">
+      <div className="container p-4 detail">
+        <div className="row">
+          <div className="col-3 p-0">
             {chats.map((chat) => {
-              chatroomid = chat.chatRoomId;
-              setTimeout(chatRoom, 5000);
-              clearTimeout(chatRoom);
+              console.log(chat);
               return (
-                <ul class="list-group chat-list">
-                  <Button
-                    onClick={chatRoom}
-                    class="list-group-item"
-                    data-arg1={chat.chatRoomId}
-                  >
-                    <h6>{chat.opponentUserName} 님과의채팅</h6>
-                  </Button>
-                </ul>
+                <button
+                  data-msg1={`${chat.chatRoomId}`}
+                  data-msg2={`${chat.opponentUserName}`}
+                  data-msg3={`${user.nickname}`}
+                  onClick={chatRoom}
+                >
+                  {chat.opponentUserName} 님과의채팅
+                </button>
               );
             })}
           </div>
-          <div class="col-9 p-0">
-            <div class="chat-room">
-              <ul class="list-group chat-content" id="chat-content">
+          <div className="col-9 p-0">
+            <div className="chat-room">
+              <ul className="list-group chat-content" id="chat-content">
                 {messages
                   .map((msg) => {
-                    return msg.writer.id == user.id ? (
+                    return msg.writer.id === user.id ? (
                       <li>
-                        <span class="chat-box mine">{msg.message}</span>
+                        <span className="chat-box mine">{msg.message}</span>
                       </li>
                     ) : (
                       <li>
-                        <span class="chat-box">{msg.message}</span>
+                        <span className="chat-box">{msg.message}</span>
                       </li>
                     );
                   })
                   .reverse()}
               </ul>
-              <div class="input-group">
+              <div className="input-group">
                 <Form onFinish={onClickPurchase}>
                   <Form.Item name="message">
                     <Input
-                      className="upload-name"
                       size="large"
                       placeholder="메세지를 입력해주세요."
                     ></Input>
